0346005a69a89e7bd55e8851b67e5eb75e6e3825
nqs2kq-spec <nq.s2.kq@gmail.com>
2026-05-15 17:15:34 +0900

    Update reservation.py

--- cogs/cogs/reservation.py
+++ cogs/cogs/reservation.py
@@ -1,1 +1,12 @@
+from discord.ext import commands
 
+class Reservation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("cog動いてるよ")
+
+async def setup(bot):
+    await bot.add_cog(Reservation(bot))
